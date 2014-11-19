9cee00879bd1aa2d776d8579fc68f0c822c5a19b
WKawin <kawin.w.1992@gmail.com>
2014-11-20 00:51:52 +0700

    ยังดูหน้า viewItem ไม่ได้ และ controller รอ service ของ item, comment

--- src/main/java/com/se/ebid/controller/ViewItemController.java
+++ src/main/java/com/se/ebid/controller/ViewItemController.java
@@ -5,9 +5,16 @@
  */
 package com.se.ebid.controller;
 
+import com.se.ebid.entity.Item;
+import com.se.ebid.entity.Photo;
+import com.se.ebid.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -15,18 +22,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ViewItemController {
-    @RequestMapping("/viewItem")
-     public String viewEditPaymentInfo(Model model) {
-        model.addAttribute("title", "View Item");
+    private ItemService itemService; //waiting for declaring itemService
+    //private CommentService commentService;
+    
+  /*  @Autowired
+    public void setItemService(ItemService itemService){
+        this.itemService = itemService;
+    }
+    
+    @Autowired
+    public void setCommentService(CommentService commentService){
+        this.commentService = commentService;
+    }
+    */
+    @RequestMapping(value = "/viewItem/{itemID}",method = RequestMethod.GET)
+     public String viewItem(@PathVariable("itemID") long itemID, Model model) {
+        QuestionForm qform = new QuestionForm();
+        qform.setItemID(itemID);
+        model.addAttribute("qform",qform);
+        model.addAttribute("item",this.itemService.getItem(itemID));
+      //  model.addAttribute("listPhotos",this.itemService.getPhoto(itemID));
+       // model.addAttribute("listComments",this.itemService.getComment(itemID));
         return "viewItemView";
     }  
     
-     public void askQuestion(){
-         //di sth
-     }
-     public void onSubmitQuestionForm(QuestionForm form){
-         //do sth
-     }
+     @RequestMapping(value = "/viewItem/onSubmitQuestionForm",method = RequestMethod.POST)
+     public String onSubmitQuestionForm(@ModelAttribute QuestionForm qform){
+    //     this.commentService.askQuestion(qform);
+    //     return "redirect:/viewItem";
+         return "redirect:/viewItemView";
+     } 
      public void onSubmitBidForm(BidForm form){
          //do sth
      }

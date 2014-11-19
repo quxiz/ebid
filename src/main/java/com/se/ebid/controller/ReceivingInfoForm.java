a68538b4e05b3e85114bed24de415e68b32437f0
overmindz <iqmathematics@gmail.com>
2014-11-19 14:55:36 +0700

    minor fix - file name misspelling
    Recieving -> Receiving

---
+++ src/main/java/com/se/ebid/controller/ReceivingInfoForm.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se.ebid.controller;
+
+/**
+ *
+ * @author mtmmoei
+ */
+public class ReceivingInfoForm {
+    private String payPalAccount;
+
+    public String getPayPalAccount() {
+        return payPalAccount;
+    }
+
+    public void setPayPalAccount(String payPalAccount) {
+        this.payPalAccount = payPalAccount;
+    }
+    
+}

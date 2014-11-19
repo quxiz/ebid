d15977a47f4065c6e13aa9dbf39f599adf92dfe5
Mimosha <mtmmoei@gmail.com>
2014-11-19 20:04:44 +0700

    register น่าจะได้ละ ยังไม่ได้ลองเทส รอ service

--- src/main/java/com/se/ebid/controller/RegisterController.java
+++ src/main/java/com/se/ebid/controller/RegisterController.java
@@ -5,8 +5,10 @@
  */
 package com.se.ebid.controller;
 
+import com.se.ebid.entity.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,14 +18,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class RegisterController {
+
     @RequestMapping("/register")
-     public String viewRegister(Model model) {
+    public String viewRegister(Model model) {
+        model.addAttribute("registrationForm", new RegistrationForm());
         model.addAttribute("countryList", new CountryList());
         model.addAttribute("title", "Register");
         return "registerView";
     }
-    @RequestMapping(value= "/register/submit", method = RequestMethod.POST)
-     public void onSubmit(RegistrationForm form){
-         //do something
-     }
+
+    @RequestMapping(value = "/register/submit", method = RequestMethod.POST)
+    public String onSubmit(@ModelAttribute RegistrationForm form) {
+//         this.MemberService.register(form);
+        return "redirect:/";
+    }
 }

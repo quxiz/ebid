9791400a3f857274f45db43b4218fe7f248c8ce0
WKawin <kawin.w.1992@gmail.com>
2014-11-20 21:08:55 +0700

    no message

--- src/main/java/com/se/ebid/controller/ViewItemController.java
+++ src/main/java/com/se/ebid/controller/ViewItemController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -49,6 +50,47 @@ public class ViewItemController {
         // model.addAttribute("listComments",this.itemService.getComment(itemID));
         return "viewItemView"; //jsp
     }
+    
+    
+
+    @RequestMapping(value = "/viewItem/onSubmitQuestionForm", method = RequestMethod.POST)
+    public String onSubmitQuestionForm(@ModelAttribute QuestionForm qform) {
+        //     this.commentService.askQuestion(qform);
+        return "redirect:/viewItem"; //url
+    }
+
+    public void onSubmitBidForm(BidForm form) {
+        //do sth
+    }
+    
+    
+    /*
+    @RequestMapping(value = "/viewItem/onSubmitBuyForm", method = RequestMethod.POST)
+    public String onSubmitBuyForm(@ModelAttribute BuyForm buyform) {
+        
+        return "redirect:/buyItem/"+ buyform.getItemID() +"/" + buyform.getQuantity();
+    }
+    */
+    @RequestMapping(value = { "/viewItem/onSubmitBuyForm" }, method = RequestMethod.POST)
+    public String onSubmitBuyForm(@ModelAttribute("buyform") BuyForm buyform,
+        Model model, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("buyform", buyform);
+
+        return "redirect:/buyItem/"+buyform.getItemID();
+    }
+    
+    @RequestMapping(value = "/buyItem/{itemID}", method = RequestMethod.GET)
+    public String buyItem(@ModelAttribute("buyform") BuyForm buyform,@PathVariable("itemID") long itemID,Model model) {
+        Invoice invoice = this.itemService.buy(buyform);
+        model.addAttribute("buyform", buyform);
+        model.addAttribute("invoice", invoice);
+        model.addAttribute("item", this.itemService.getItem(buyform.getItemID()));
+       // model.addAttribute("listPhotos", this.itemService.getItem(buyform.getItemID()).getPhoto());
+        return "buyItemView";  
+    } 
+    /*
+    
     //not sure
     @RequestMapping(value = "/buyItem/{itemID}/{quantity}", method = RequestMethod.GET)
     public String buyItem(@PathVariable("itemID") long itemID,@PathVariable("quantity") long quantity, Model model) {
@@ -61,27 +103,13 @@ public class ViewItemController {
         model.addAttribute("item", this.itemService.getItem(itemID));
      //   model.addAttribute("listPhotos",this.itemService.getPhoto(itemID));
         return "buyItemView"; //jsp
-    }
+    }*/
+    
+    
     
 
-    @RequestMapping(value = "/viewItem/onSubmitQuestionForm", method = RequestMethod.POST)
-    public String onSubmitQuestionForm(@ModelAttribute QuestionForm qform) {
-        //     this.commentService.askQuestion(qform);
-        return "redirect:/viewItem"; //url
-    }
-
-    public void onSubmitBidForm(BidForm form) {
-        //do sth
-    }
-
-    @RequestMapping(value = "/viewItem/onSubmitBuyForm", method = RequestMethod.POST)
-    public String onSubmitBuyForm(@ModelAttribute BuyForm buyform) {
-        
-        return "redirect:/buyItem/"+buyform.getItemID()+"/"+buyform.getQuantity();
-    }
-
      @RequestMapping(value = "/buyItem/confirmBuy", method = RequestMethod.POST)
-    public String confirmBuy() {
+    public String confirmBuy(@ModelAttribute BuyForm buyform) {
         
         
         return "";

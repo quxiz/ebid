cfe1380496410ce80092b26fa26c990445277383
WKawin <kawin.w.1992@gmail.com>
2014-11-21 18:03:47 +0700

    comment โค้ดไว้ก่อนรอคลาส service

--- src/main/java/com/se/ebid/controller/ReportController.java
+++ src/main/java/com/se/ebid/controller/ReportController.java
@@ -10,7 +10,10 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -18,14 +21,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ReportController {
-    @RequestMapping("/report")
-     public String viewEditPaymentInfo(Model model) {
+    //comment ให้รันผ่านเฉยๆเพราะกุยังไม่มี service
+  /*  private ReportService reportService;
+    
+    @Autowired
+     public void setReportService(ReportService reportService) {
+        this.reportService = reportService;
+    }
+     
+    @RequestMapping(value = "/report/", method = RequestMethod.GET)
+     public String viewReport(Model model) {
         model.addAttribute("title", "Report");
-         List<CategoryType> categoryList = new ArrayList<CategoryType>( Arrays.asList(CategoryType.values() ));  
+        List<CategoryType> categoryList = new ArrayList<>( Arrays.asList(CategoryType.values() ));  
         model.addAttribute("categoryList", categoryList);
+        ReportForm reportForm = new ReportForm();
+        model.addAttribute("reportForm",reportForm);
         return "reportView";
     }  
-     public void printReport(String s,String t){
+     
+     @RequestMapping(value = "/report/printReport", method = RequestMethod.POST)
+     public String printReport(@ModelAttribute("report") Report report,@ModelAttribute ReportForm reportForm,
+            Model model, RedirectAttributes redirectAttributes){
          //do sth
+         report = this.reportService.printReport(reportForm.getReportType(),reportForm.getMonth(),reportForm.getYear());
+         redirectAttributes.addFlashAttribute("report", report);
+         return "redirect:/repoer/showReportView";
      }
+     
+     @RequestMapping(value = "/reportView/printReport", method = RequestMethod.GET)
+     public String showReport(@ModelAttribute("report") Report report, Model model){
+         model.addAttribute("report",report);
+         return "reportView";
+     } */
 }
---
+++ src/main/java/com/se/ebid/controller/ReportForm.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.se.ebid.controller;
+
+/**
+ *
+ * @author Kawin
+ */
+public class ReportForm {
+    private int year;
+    private int month;
+    ReportType reportType;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public ReportType getReportType() {
+        return reportType;
+    }
+
+    public void setReportType(ReportType reportType) {
+        this.reportType = reportType;
+    }
+    
+    
+}

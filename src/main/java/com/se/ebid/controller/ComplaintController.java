505250cbd4b33b07750b4ff57fcf5eeb182d174a
WKawin <kawin.w.1992@gmail.com>
2014-11-21 20:47:54 +0700

    complaintcontroller

--- src/main/java/com/se/ebid/controller/ComplaintController.java
+++ src/main/java/com/se/ebid/controller/ComplaintController.java
@@ -5,12 +5,17 @@
  */
 package com.se.ebid.controller;
 
+import com.se.ebid.service.ComplaintService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -18,14 +23,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ComplaintController {
-    @RequestMapping("/complaint")
+    
+      private ComplaintService complaintService;
+      
+      @Autowired
+      public void setComplaintService(ComplaintService complaintService){
+          this.complaintService = complaintService;
+      }
+       
+    @RequestMapping(value = "/complaint", method = RequestMethod.GET)
      public String viewComplaint(Model model) {
         model.addAttribute("title", "Complaint");
-         List<CategoryType> categoryList = new ArrayList<CategoryType>( Arrays.asList(CategoryType.values() ));  
+         List<CategoryType> categoryList = new ArrayList<>( Arrays.asList(CategoryType.values() ));  
         model.addAttribute("categoryList", categoryList);
+        ComplaintForm complaintForm = new ComplaintForm();
+        model.addAttribute("complaintForm", complaintForm);
         return "homeView";
     }  
-     public void onSubmit(ComplaintForm form){
-         //do sth
+     
+     @RequestMapping(value = "/complaint", method = RequestMethod.POST)
+     public String onSubmit(@ModelAttribute ComplaintForm complaintForm){
+         this.complaintService.complaint(complaintForm);
+         return "redirect:/";
      }
 }

39509e370b15b1137ddc443e2e25c794220d35dd
yumeyami <iscream.p@gmail.com>
2014-11-21 16:20:59 +0700

    edit ViewSellerController

--- src/main/java/com/se/ebid/controller/ViewSellerController.java
+++ src/main/java/com/se/ebid/controller/ViewSellerController.java
@@ -5,12 +5,17 @@
  */
 package com.se.ebid.controller;
 
+import com.se.ebid.service.MemberService;
+import com.sun.corba.se.impl.interceptors.PINoOpHandlerImpl;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -18,13 +23,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ViewSellerController {
-    @RequestMapping("/viewSeller")
-     public String viewSeller(Model model) {
+
+    private MemberService memberService;
+
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @RequestMapping(value = "/viewSeller/{sellerID}", method = RequestMethod.GET)
+    public String viewSeller(@PathVariable("sellerID") long sellerID, String sellerName, Model model) {
         model.addAttribute("title", "ข้อมูลผู้ขาย");
-         List<CategoryType> categoryList = new ArrayList<CategoryType>( Arrays.asList(CategoryType.values() ));  
+        model.addAttribute("seller", this.memberService.getSeller(sellerName));
+        model.addAttribute("sellerFeedBack", this.memberService.getSellerFeedback(sellerID));
+        List<CategoryType> categoryList = new ArrayList<CategoryType>(Arrays.asList(CategoryType.values()));
         model.addAttribute("categoryList", categoryList);
         return "viewSellerView";
     }
-    
-    
 }

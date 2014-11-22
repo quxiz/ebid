9d2df7a21661b4d90e769258b3442dbad1bdda6d
WKawin <kawin.w.1992@gmail.com>
2014-11-22 14:37:43 +0700

    many controllers updated

--- src/main/java/com/se/ebid/controller/EditPaymentInfoController.java
+++ src/main/java/com/se/ebid/controller/EditPaymentInfoController.java
@@ -5,12 +5,16 @@
  */
 package com.se.ebid.controller;
 
+import com.se.ebid.entity.Member;
+import com.se.ebid.service.MemberService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -18,14 +22,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class EditPaymentInfoController {
+    
+    private MemberService memberService;
+    public void setMemberService(MemberService memberservice){
+        this.memberService=memberService;
+    }
      @RequestMapping("/editPaymentInfo")
      public String viewEditPaymentInfo(Model model) {
         model.addAttribute("title", "Edit Payment Info");
-         List<CategoryType> categoryList = new ArrayList<CategoryType>( Arrays.asList(CategoryType.values() ));  
+         List<CategoryType> categoryList = new ArrayList<>( Arrays.asList(CategoryType.values() ));  
         model.addAttribute("categoryList", categoryList);
+        Member member = this.memberService.getMember();
+        PaymentInfoForm paymentInfoForm = new PaymentInfoForm();
+        model.addAttribute("member", member);
+        model.addAttribute("paymentInfoForm", paymentInfoForm);
         return "editPaymentInfoView";
     }  
-     public void onSubmit(PaymentInfoForm form){
-         //do sth
+     
+     @RequestMapping(value = "/editPaymentInfo/onSubmit",method = RequestMethod.POST)
+     public String onSubmit(@ModelAttribute PaymentInfoForm paymentInfoForm){
+         this.memberService.editPaymentInfo(paymentInfoForm);
+         return "redirect:/";//หน้าแก้ไขสำเร็จ
      }
 }

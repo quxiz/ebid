5ea80d61bec107a7c020e2929a9a5551202b289d
Quxiz <quxiz@hotmail.com>
2014-11-27 17:52:46 +0700

    admin is not user now

--- src/main/java/com/se/ebid/service/MyUserDetailsService.java
+++ src/main/java/com/se/ebid/service/MyUserDetailsService.java
@@ -32,8 +32,10 @@ public class MyUserDetailsService implements UserDetailsService {
 
         Member member = memberDAO.findByUserID(userID);
         Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        if(userID.equals("demo") || userID.equals("admin")) {
+        if (!userID.equals("admin")) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        if (userID.equals("demo") || userID.equals("admin")) {
             System.out.println("ADMIN");
             authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
         }
